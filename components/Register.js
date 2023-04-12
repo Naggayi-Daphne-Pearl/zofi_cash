33d566ae547774d1513080ea44fb55f200897104
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-12 09:58:19 +0300

    Vaidation for a Register page using formik and yup

--- components/Register.js
+++ components/Register.js
@@ -1,93 +1,178 @@
-import React, { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import validationSchema from "./Schema";
 import Button from "./Button";
-import { useAuth } from "../context/AuthContext";
-import { countries } from "./countries";
 
-const KYCPage = () => {
-  const [selectedOption, setSelectedOption] = useState(countries[0].value);
+function Register() {
+  const initialValues = { email: "", password: "" };
 
-  const handleSelectChange = (event) => {
-    setSelectedOption(event.target.value);
+  const handleSubmit = (values) => {
+    // handle form submission
   };
 
   return (
-    <div>
-      <div className="flex flex-col items-center min-h-screen pt-4 sm:justify-center sm:pt-0 bg-gray-50">
-        <div className="w-full px-6 py-8 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
-          <form>
-            <div class="grid grid-cols-2 gap-10">
-              <div class="h-30">
-                <label className="flex justify-center p-2 ">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  className="block border-primary w-full mt-1 border-gray-70 rounded-md shadow-md  focus:primary  p-2"
-                />
-              </div>
-              <div class="h-30">
-                <label className="flex justify-center p-2 ">Roles</label>
-                <select
-                  name="roles"
-                  class="block border-primary w-full mt-1 border-gray-70 rounded-md shadow-md  focus:ring-primary focus:ring-opacity-50 p-2"
+    <div className="flex justify-center items-center h-screen">
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched }) => (
+          <Form className="bg-white shadow-md rounded px-20 pt-6 pb-8 mb-4">
+            <h1 className="flex justify-center text-3xl text-primary py-8">
+              REGISTER FORM
+            </h1>
+            <div className="grid grid-cols-2 gap-10">
+              <div className="mb-1">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-1 text-xl justify-center flex pb-2"
                 >
-                  <option class="text-gray-700 hover:bg-gray-50 focus:bg-gray-10">
-                    Employer
-                  </option>
-                  <option class="text-gray-700 hover:bg-gray-100 focus:bg-gray-100">
-                    Employee
-                  </option>
-                  <option class="text-gray-700 hover:bg-gray-100 focus:bg-gray-100">
-                    Payroll
-                  </option>
-                  <option class="text-gray-700 hover:bg-gray-100 focus:bg-gray-100">
-                    Staker
-                  </option>
-                </select>
-              </div>
-              <div class="h-30">
-                <label className="flex justify-center p-2 ">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  className="block border-primary w-full mt-1 border-gray-70 rounded-md shadow-md  focus:ring-primary focus:ring-opacity-50 p-2"
-                />
-              </div>
-              <div class="h-30">
-                <label className="flex justify-center p-2 ">
-                  Repeat Password
+                  Email
                 </label>
-                <input
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  className={
+                    errors.email && touched.email
+                      ? "border-red-500 appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      : "appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  }
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+              <div className="mb-1">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-1 text-xl justify-center flex pb-1"
+                >
+                  Roles
+                </label>
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  className={
+                    errors.email && touched.email
+                      ? "border-red-500 appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      : "appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  }
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+              <div className="mb-1">
+                <label
+                  htmlFor="password"
+                  className="block text-gray-700 font-bold mb-1 text-xl justify-center flex pb-1"
+                >
+                  Password
+                </label>
+                <Field
                   type="password"
-                  name="repeat_password"
-                  className="block border-primary w-full mt-1 border-gray-70 rounded-md shadow-md  focus:ring-primary focus:ring-opacity-50 p-2"
+                  id="password"
+                  name="password"
+                  className={
+                    errors.password && touched.password
+                      ? "border-red-500 appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      : "appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  }
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-500 text-xs italic"
                 />
               </div>
-              <div class="h-30">
-                <label className="flex justify-center p-2 ">Country Code</label>
-                <input
-                  type="text"
-                  name="country_code"
-                  className="block border-primary w-full mt-1 border-gray-70 rounded-md shadow-md  focus:ring-primary focus:ring-opacity-50 p-2"
+
+              <div className="mb-1">
+                <label
+                  htmlFor="confirmPassword"
+                  className="block text-gray-700 font-bold mb-1 text-xl justify-center flex pb-1"
+                >
+                  Confirm Password
+                </label>
+                <Field
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  className={
+                    errors.confirmPassword && touched.confirmPassword
+                      ? "border-red-500 appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      : "appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  }
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className="text-red-500 text-xs italic"
                 />
               </div>
-              <div class="h-30">
-                <label className="flex justify-center p-2 ">Address</label>
-                <input
-                  type="address"
-                  name="ip"
-                  className="block border-primary w-full mt-1 border-gray-70 rounded-md shadow-md  focus:ring-primary focus:ring-opacity-50 p-2"
+
+              <div className="mb-1">
+                <label
+                  htmlFor="phoneNumber"
+                  className="block text-gray-700 font-bold mb-1 text-xl justify-center flex pb-2"
+                >
+                  Phone Number
+                </label>
+                <Field
+                  type="tel"
+                  id="phoneNumber"
+                  name="phone"
+                  className={
+                    errors.phoneNumber && touched.phoneNumber
+                      ? "border-red-500 appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      : "appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  }
+                />
+                <ErrorMessage
+                  name="phoneNumber"
+                  component="div"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+
+              <div className="mb-1">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-1 text-xl justify-center flex pb-2"
+                >
+                  Country
+                </label>
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  className={
+                    errors.email && touched.email
+                      ? "border-red-500 appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      : "appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  }
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-xs italic"
                 />
               </div>
             </div>
 
-            <div className=" pt-8 flex justify-center">
-              <Button>Continue</Button>
+            <div className="flex items-center justify-center mt-6 ">
+              <Button type="submit">REGISTER</Button>
             </div>
-          </form>
-        </div>
-      </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-};
+}
 
-export default KYCPage;
+export default Register;
