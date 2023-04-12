31b7b43673f19447df39b3f0b1cb6ff59f9d8fa1
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-12 15:24:16 +0300

    login route

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,10 @@
 import LoginPage from "@/components/LoginPage";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function Login() {
   return (
     <div>
+      <ToastContainer />
       <LoginPage />
     </div>
   );
