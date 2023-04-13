508b8619f032fc6753abfcff3fb0c6835787c213
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-14 00:19:22 +0300

    Forgot Paassword

--- components/ForgotPassword.js
+++ components/ForgotPassword.js
@@ -4,9 +4,23 @@ import validationSchema from "./Schema";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import OtpVerfication from "./OtpVerficiationForm";
 import { useAuth } from "../contexts/AuthContext";
+import { useRouter } from "next/router";
 
 const ForgotPassword = () => {
-  const initialValues = { email: "" };
+  const initialValues = { security_answer: "", security_question: "" };
+
+  const router = useRouter();
+
+  const questions = [
+    { question: "What is your city of birth?" },
+    { question: "What is your favorite color?" },
+    { question: "What is your childhood nickname?" },
+  ];
+  const [selectQuestion, setSelectQuestion] = useState("");
+  function handleChangeQuestion(event) {
+    setSelectQuestion(event.target.value);
+  }
+  const { setSecurityAnswer } = useAuth();
 
   const { forgotPassword, error } = useAuth();
   const [email, setEmail] = useState("");
@@ -23,59 +37,83 @@ const ForgotPassword = () => {
   return (
     <div>
       <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 ">
-        <div>
-          <a href="https://www.zoficash.com/">
-            <h3 className="text-5xl font-bold text-#335EEA font-header text-primary py-6">
-              Zofi Cash
-            </h3>
-          </a>
-        </div>
-
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          {({ errors, touched }) => (
-            <Form className="bg-white shadow-md rounded px-20 pt-6 pb-10 mb-4">
-              <h1 className="flex justify-center text-3xl text-primary py-8">
-                Forgot Password?
-              </h1>
-              <div className="mb-6">
-                <label
-                  htmlFor="email"
-                  className="block text-gray-700 font-bold mb-2 text-2xl justify-center flex pb-3"
-                >
-                  Email
-                </label>
-                <Field
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Enter your email"
-                  className={
-                    errors.email && touched.email
-                      ? "border-red-500 appearance-none border rounded w-full py-3 px-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                      : "appearance-none border rounded w-full py-3 px-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
-                  }
-                />
-                <ErrorMessage
-                  name="email"
-                  component="div"
-                  className="text-red-500 text-xs italic"
-                />
-              </div>
-              {error && <div>{error}</div>}
-              <div className="flex items-center justify-center ">
-                <a href="/auth/resetpassword">
-                  <Button type="submit" onSubmit={handleSubmit}>
-                    Reset Password
+        <h1 className="flex justify-center sm:text-3xl text-5xl text-primary py-10 text-header font-bold">
+          Zofi Cash
+        </h1>
+        <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-lg sm:rounded-lg">
+          <h1 className="flex justify-center text-3xl text-primary py-10 font-semi">
+            Reset Password
+          </h1>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting, errors, touched }) => (
+              <Form>
+                <div className="mt-4">
+                  <label
+                    name="security_question"
+                    className="block text-md font-medium text-gray-700 font-body flex justify-center text-xl p-2 "
+                  >
+                    <Field
+                      as="select"
+                      id="security_question"
+                      name="security_question"
+                      value={selectQuestion}
+                      onChange={handleChangeQuestion}
+                      className="appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    >
+                      <option value="">Select Security Answer</option>
+                      {questions.map((role) => (
+                        <option
+                          key={role.question}
+                          value={role.security_question}
+                        >
+                          {role.question}
+                        </option>
+                      ))}
+                    </Field>
+                  </label>
+                  <div
+                    name="security_answer"
+                    className="flex flex-col items-start"
+                  >
+                    <label
+                      htmlFor="security_answer"
+                      className="block text-gray-700 font-bold mb-1 text-xl justify-center flex pb-2"
+                    ></label>
+                    <Field
+                      type="tel"
+                      id="security_answer"
+                      name="security_answer"
+                      placeholder="Enter your answer"
+                      className={
+                        errors.security_answer && touched.security_answer
+                          ? "border-red-500 appearance-none border rounded w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          : "appearance-none border rounded w-full py-2 px-6 text-gray-700 focus:outline-none focus:shadow-outline"
+                      }
+                    />
+                    <ErrorMessage
+                      name="security_answer"
+                      component="div"
+                      className="text-red-500 text-xs italic"
+                    />
+                  </div>
+                </div>
+                <div className=" pt-8 flex justify-center">
+                  <Button
+                    type="submit"
+                    className="flex items-center justify-between mt-8"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? "Submitting..." : "Reset"}
                   </Button>
-                </a>
-              </div>
-            </Form>
-          )}
-        </Formik>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
     </div>
   );
