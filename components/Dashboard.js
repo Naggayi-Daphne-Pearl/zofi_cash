7261fd150d01835cf2228d93fa062d600e77bbbb
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-13 20:30:34 +0300

    First Page and index page

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen ">
+      <div className="display-block px-6">
+        <h1 className="text-primary text-5xl">WELCOME TO ZOFI CASH</h1>
+        <a href="/auth/signup">
+          <h1 className="text-primary text-2xl underline py-12">
+            Click here to register!!
+          </h1>
+        </a>
+        <footer className="mt-12 pt-12">
+          © Copyright 2023. All right reserved, Naggayi-Daphne-Pearl
+        </footer>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
