2efb59573506efa390e02e6d5f8cea933789c59d
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-13 10:44:28 +0300

    validation schema

--- components/Schema.js
+++ components/Schema.js
@@ -4,19 +4,22 @@ import { countryCodes } from "./data";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object({
-  phoneNumber: Yup.string()
-    .matches(/^[0-9]{10}$/, "Please enter a valid phone number"),
-    // .required("Phone number is required"),
+  phone_number: Yup.string().matches(
+    /^[0-9]{10}$/,
+    "Please enter a valid phone number"
+  ),
+  // .required("Phone number is required"),
 
   email: Yup.string().email("Invalid email address").required("Required"),
   password: Yup.string()
     .min(8, "Password must be at least 8 characters")
     .required("Required"),
-  confirmPassword: Yup.string()
+  repeat_password: Yup.string()
     .oneOf([Yup.ref("password"), null], "Passwords must match")
     .required("Required"),
-  countryCode: Yup.string(),
+  country_code: Yup.string(),
   role: Yup.string(),
+  security_answer: Yup.string().required("Required"),
 });
 
 export default validationSchema;
