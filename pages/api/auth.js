bdeb29f1ae58c746bb9b8c5739c5f4b8f8cdcbfa
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-13 23:36:52 +0300

    Set up security answer api

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -4,11 +4,14 @@
 const LOGIN_URL = "https://staging-auth-api.zoficash.com/api/v1/account-login";
 const REGISTER_URL =
   "https://staging-auth-api.zoficash.com/api/v1//account-registration";
-const VERIFY_EMAIL_URL = "https://staging-auth-api.zoficash.com/api/v1/send-email";
+const VERIFY_EMAIL_URL =
+  "https://staging-auth-api.zoficash.com/api/v1/send-email";
 const VERIFY_PHONE_URL =
   "https://staging-auth-api.zoficash.com/api/v1/send-sms";
 const RESET_PASSWORD_URL =
   "https://staging-auth-api.zoficash.com/api/v1/user-reset-account-password/";
+const SET_SECURITY_ANSWER_URL =
+  "https://staging-auth-api.zoficash.com/api/v1/setup-security-question/:security_question_setup_id";
 
 // Function to log in the user
 export const loginApi = async (email, password) => {
@@ -100,6 +103,32 @@ export const resetPasswordApi = async (
       newPassword,
     }),
   });
+
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+};
+
+// Set up Security Answer
+export const setSecurityAnswerApi = async (
+  securityQuestionSetupId,
+  securityAnswer
+) => {
+  const response = await fetch(
+    SET_SECURITY_ANSWER_URL.replace(
+      ":security_question_setup_id",
+      securityQuestionSetupId
+    ),
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify({ securityAnswer }),
+    }
+  );
 
   if (!response.ok) {
     const errorMessage = await response.text();
