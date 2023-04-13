7b67facc8d8ab63f888bed92b5cc8c3d73b715fa
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-13 10:42:18 +0300

    Verify email account

---
+++ components/VerifyAccount.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import validationSchema from "./Schema";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const VerifyAccount = () => {
+  const initialValues = { email: "" };
+  const [otpSent, setOtpSent] = useState(false);
+
+  const handleSubmit = async (values, { setSubmitting, setErrors }) => {
+    try {
+      const response = await fetch(
+        "https://staging-auth-api.zoficash.com/api/v1/send-email",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email: values.email }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to send verification email.");
+      }
+      setOtpSent(true);
+    } catch (error) {
+      setErrors({ email: error.message });
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 ">
+        <div>
+          <a href="https://www.zoficash.com/">
+            <h3 className="text-5xl font-bold text-#335EEA font-header text-primary py-6">
+              Zofi Cash
+            </h3>
+          </a>
+        </div>
+
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched }) => (
+            <Form className="bg-white shadow-md rounded px-20 pt-6 pb-10 mb-4">
+              <h1 className="flex justify-center text-3xl text-primary py-8">
+                Sign Up
+              </h1>
+              <div className="mb-6">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-bold mb-2 text-2xl justify-center flex pb-3"
+                >
+                  Email
+                </label>
+                <Field
+                  type="tel"
+                  id="email"
+                  name="phone"
+                  className={
+                    errors.email && touched.email
+                      ? "border-red-500 appearance-none border rounded w-full py-3 px-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      : "appearance-none border rounded w-full py-3 px-10 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+                  }
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-xs italic"
+                />
+              </div>
+              <div className="flex items-center justify-center ">
+                <Button type="submit" onSubmit={handleSubmit}>
+                  Verify Email
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyAccount;
