eba37e7221aaf677b496c39b24afbac3a383b426
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-14 12:52:57 +0300

    Dashboard

---
+++ components/Dashboard.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import { BsPower, BsFillPersonFill } from "react-icons/bs";
+const Dashboard = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <nav className="flex justify-between px-20 bg-white shadow-md pt-4">
+        <div>
+          <a href="https://www.zoficash.com/">
+            <h1 className=" text-2xl text-primary pt-2 pb-3 px-12 font-bold italic hover:underline">
+              Zofi cash
+            </h1>
+          </a>
+        </div>
+        <div className="relative">
+          <button
+            onClick={toggleDropdown}
+            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded"
+          >
+            <BsFillPersonFill />
+          </button>
+          {isOpen && (
+            <div className=" absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-lg z-10">
+              <a
+                href="/auth/logout"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white"
+              >
+                Log Out
+              </a>
+              <a
+                href="/auth/deactivate"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white"
+              >
+                Deactivate account
+              </a>
+            </div>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Dashboard;
