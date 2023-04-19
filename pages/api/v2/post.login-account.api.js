b296caca10c94fe53068def793e3f8cea8986503
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-19 10:22:16 +0300

    handle loginUser api

---
+++ pages/api/v2/post.login-account.api.js
@@ -0,0 +1,15 @@
+export default async function loginUser(req, res) {
+  const apiUrl = process.env.LOGIN_URL;
+  if (req.method !== "POST")
+    return res.json({ status: 400, errors: `${req.method} not allowed.` });
+  try {
+    fetch(apiUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+  } catch (error) {
+    console.log("error", error);
+    return res.json({ status: 400, errors: error });
+  }
+}
