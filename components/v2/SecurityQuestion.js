5434552a3f98dc60d03ce7db9d19a1f0ab8d7880
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-19 12:41:40 +0300

    Refactor code

--- components/v2/SecurityQuestion.js
+++ components/v2/SecurityQuestion.js
@@ -5,7 +5,7 @@ import Button from "../Button";
 import { useRouter } from "next/router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useAuth } from "../../contexts/v1/AuthContext";
+import { useAuth } from "../../contexts/v2/AuthContext";
 
 const SecurityQuestion = () => {
   const initialValues = { security_answer: "", security_question: "" };
