5bc332bf9690b75c5f2c7ccbd2b29dabc7cba1d5
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-19 13:41:15 +0300

    Handle Submit for forgot password

--- contexts/v2/AuthContext.js
+++ contexts/v2/AuthContext.js
@@ -9,9 +9,10 @@ import { createContext, useContext, useState } from "react";
 // } from "../../pages/api/v1/auth";
 import { registerUser } from "../../pages/api/v2/post.register-account.api";
 import { loginUser } from "../../pages/api/v2/post.login-account.api";
-import { resetPasswordApi } from "../../pages/api/v2/get.reset-password.api";
+import { resetPasswordApi } from "../../pages/api/v2/post.reset-password.api";
 import { securityAnswerApi } from "../../pages/api/v2/post.security-question.api";
 import { verifyAccountApi } from "../../pages/api/v2/get.verify-account.api";
+import { forgotPasswordApi } from "../../pages/api/v2/get.forgot-password.api";
 
 const AuthContext = createContext();
 
@@ -44,19 +45,9 @@ export const AuthProvider = ({ children }) => {
   };
 
   // resetting password
-  const resetPassword = async (
-    email,
-    security_question,
-    security_answer,
-    newPassword
-  ) => {
+  const resetPassword = async (password) => {
     try {
-      await resetPasswordApi(
-        email,
-        security_question,
-        security_answer,
-        newPassword
-      );
+      await resetPasswordApi(password);
       setError(null);
     } catch (error) {
       setError("Failed to reset password");
@@ -64,25 +55,15 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  //   const forgotPassword = async (email) => {
-  //     try {
-  //       await forgotPasswordApi(email);
-  //       setError(null);
-  //     } catch (error) {
-  //       setError("Failed to reset password");
-  //       throw error;
-  //     }
-  //   };
-
-  //   const setSecurityAnswer = async (security_question, security_answer) => {
-  //     try {
-  //       await setSecurityAnswerApi(security_question, security_answer);
-  //       setError(null);
-  //     } catch (error) {
-  //       setError("Failed to set security answer");
-  //       throw error;
-  //     }
-  //   };
+  const forgotPassword = async (security_answer, security_question ) => {
+    try {
+      await forgotPasswordApi(security_answer, security_question);
+      setError(null);
+    } catch (error) {
+      setError("Failed to reset password");
+      throw error;
+    }
+  };
 
   // set security answer
   const setSecurityAnswer = async (security_question, security_answer) => {
@@ -116,7 +97,7 @@ export const AuthProvider = ({ children }) => {
         error,
         login,
         register,
-        // forgotPassword,
+        forgotPassword,
         logout,
         resetPassword,
         setSecurityAnswer,
