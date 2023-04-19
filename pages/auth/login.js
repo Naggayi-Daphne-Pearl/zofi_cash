424bec59658bbc5a564360069793ab8a46570b26
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-04-19 10:43:12 +0300

    Version 2 login page

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,4 +1,4 @@
-import LoginPage from "@/components/LoginPage";
+import LoginPage from "../../components/v2/LoginPage";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function Login() {
@@ -10,16 +10,20 @@ function Login() {
   );
 }
 
+export default Login;
+
 export async function getServerSideProps(context) {
   // fetch data from API and return as props
-  const res = await fetch("https://staging-auth-api.zoficash.com/api/v1/account-login");
+  const res = await fetch(
+    "https://staging-auth-api.zoficash.com/api/v1/account-login"
+  );
   const data = await res.json();
 
   return {
     props: {
-      data,
+      apiEndPoint: process.env.LOGIN_URL,
+      maxLoginAttempts: parseInt(process.env.MAX_LOGIN_ATTEMPTS),
+      loginLockoutTime: parseInt(process.env.LOGIN_LOCKOUT_TIME),
     },
   };
 }
-
-export default Login;
